375575539d9039c37377cd11aa0bc4ca909d77b7
0xShadowWire <0xshadowwire@protonmail.com>
2021-01-31 13:48:52 +0100

    feat: 🎸 Enable advance

--- src/components/common/EpochBlock.tsx
+++ src/components/common/EpochBlock.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
+import { useHistory } from 'react-router-dom';
 
 type EpochBlockProps = {
   epoch: string
 }
 
 function EpochBlock({ epoch }: EpochBlockProps) {
+  const history = useHistory();
+
   return (
-    <>
+    <div style={{'cursor': 'pointer'}} onClick={() => {
+      history.push('/epoch/');
+    }}>
       <div style={{ fontSize: 16, padding: 3 }}>Epoch</div>
       <div style={{ fontSize: 24, padding: 3, fontWeight: 400, lineHeight: 1.5, fontFamily: 'aragon-ui-monospace, monospace'}}>{epoch}</div>
-    </>
+    </div>
   );
 }
 
